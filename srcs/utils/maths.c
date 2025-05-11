f3f31bb8e27be683e0dd1f1d1e0a86a5f655891d
Francois <fpetit@student.42angouleme.fr>
2025-05-11 13:05:11 +0200

    matrix 1

--- srcs/utils/maths.c
+++ srcs/utils/maths.c
@@ -6,3 +6,24 @@ float   to_rad(int degrees)
 
     return (degrees * PI / 180.0f);
 }
+
+double	multiply_line(int p[3], int m[3])
+{
+
+}
+
+void	multiply_matrix(t_point *p, double matrix[3][3])
+{
+	double point[3];
+
+	point[0] = p->xf;
+	point[1] = p->yf;
+	point[2] = 1;
+
+
+}
+
+void	rotate_vector(t_point *p)
+{
+
+}
