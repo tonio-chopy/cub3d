0207787e6ed74084f3203cc0050a70c4d1fd3f20
Antonio Laualiki <alaualik@1C7.42angouleme.fr>
2025-06-09 16:25:56 +0200

    parsing of empty lines after the map fixed.

--- bonus/srcs/parse/parse_cub.c
+++ bonus/srcs/parse/parse_cub.c
@@ -35,31 +35,57 @@ MSP_MISSING);
 	}
 }
 
+static bool	is_only_whitespace(char *line)
+{
+	int	i;
+
+	if (!line)
+		return (true);
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] != ' ' && line[i] != '\t' && line[i] != '\n' && line[i] != '\r')
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
 static int	cub_parse_map(t_data *data, char **line)
 {
 	char	*trimmed;
 	int		y;
 	bool	map_started;
+	bool	map_ended;
 
 	map_started = false;
+	map_ended = false;
 	y = 0;
 	while (*line)
 	{
-		trimmed = cub_trim_full(*line);
-		if (!*trimmed)
+		if (is_only_whitespace(*line))
 		{
-			if (map_started)
-				cub_handle_fatal_parse(data, data->parsed_map->fd, *line, "Empty line in map content");
-		}
-		else if (cub_is_map_line(trimmed))
-		{
-			map_started = true;
-			cub_add_map_line(data, data->parsed_map, *line, y);
-			y++;
+			if (map_started && !map_ended)
+				map_ended = true;
 		}
 		else
 		{
-			cub_handle_fatal_parse(data, data->parsed_map->fd, *line, MSP_UNK);
+			trimmed = cub_trim_full(*line);
+			if (cub_is_map_line(trimmed))
+			{
+				if (map_ended)
+					cub_handle_fatal_parse(data, data->parsed_map->fd, *line, "Map content cannot continue after empty lines");
+				map_started = true;
+				cub_add_map_line(data, data->parsed_map, *line, y);
+				y++;
+			}
+			else
+			{
+				if (map_started)
+					cub_handle_fatal_parse(data, data->parsed_map->fd, *line, "Invalid content after map");
+				else
+					cub_handle_fatal_parse(data, data->parsed_map->fd, *line, MSP_UNK);
+			}
 		}
 		free(*line);
 		*line = get_next_line(data->parsed_map->fd);
