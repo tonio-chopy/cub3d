8e3c4fd72512d48c12d9ff43fc6ccb9551db99ea
Francois Petit <fpetit@2G6.42angouleme.fr>
2025-06-28 15:57:13 +0200

    [fix] fix bad alloc check

--- bonus/srcs/utils/clean_extra_bonus.c
+++ bonus/srcs/utils/clean_extra_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fpetit <fpetit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 17:35:54 by alaualik          #+#    #+#             */
-/*   Updated: 2025/06/21 16:40:30 by fpetit           ###   ########.fr       */
+/*   Updated: 2025/06/28 15:52:21 by fpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,8 +76,7 @@ void	cub_clean_data(t_data *data)
 {
 	if (!data)
 		return ;
-	if (data->goal || data->minimap || data->ball || data->cup || data->zbuffer)
-		cub_clean_bonus(data);
+	cub_clean_bonus(data);
 	if (data->walls)
 		cub_clean_field(data, data->walls);
 	if (data->parsed_map)
